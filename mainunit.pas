a46dd69749f1b14ef8a11f5e35a319ea17298c31
VenoM2k6 <73553759+VenoM2k6@users.noreply.github.com>
2024-02-15 06:25:14 +0100

    Fixxed Graphics

--- mainunit.pas
+++ mainunit.pas
@@ -21,6 +21,7 @@ type
     Elongation: TEdit;
     graph: TImage;
     Daempfung: TEdit;
+    Topper: TPanel;
     selectType: TRadioGroup;
     values: TStringGrid;
     procedure BerechnenClick(Sender: TObject);
@@ -65,21 +66,64 @@ begin
   closeButton.Left := screen.width div 2 - closeButton.width div 2;
   closeButton.Top := Round((graph.top + graph.height)*1.05);
 
-  values.width := Round((screen.width - (graph.left+graph.width)));
+  values.width := Round((screen.width - (graph.left+graph.width))) + screen.width div 20;
   values.height := (screen.height div 3)*2;
-  values.left := Round((graph.width+graph.left));
+  values.left := Round((graph.width+graph.left)) + values.width div 5;
   values.top := screen.height div 2 - values.height div 2;
+  values.DefaultColWidth := values.DefaultColWidth * 2;
+
+  Topper.Width := screen.width;
+  Topper.Height := values.top div 2;
+  Topper.top := 0;
+  Topper.left := 0;
+
+  selectType.height := screen.height div 8;
+  selectType.Top := screen.height div 2 - selectType.height;
+  selectType.width :=  screen.width div 7;
+  selectType.Left := screen.width div 55;
+
+  Masse.Height := screen.height div 28;
+  Masse.Width := screen.width div 14;
+  Masse.Top := selectType.top + Masse.height div 2;
+  Masse.left := selectType.left + selectType.width + Masse.width div 6;
+
+  Federkonstante.Height := screen.height div 28;
+  Federkonstante.Width := screen.width div 14;
+  Federkonstante.Top := Masse.top + Federkonstante.height;
+  Federkonstante.left := Masse.left;
+
+  Elongation.Height := screen.height div 28;
+  Elongation.Width := screen.width div 14;
+  Elongation.Top := Federkonstante.top + Elongation.height;
+  Elongation.left := Masse.left;
+
+  Daempfung.Height := screen.height div 28;
+  Daempfung.Width := screen.width div 14;
+  Daempfung.Top := Elongation.top + Elongation.height;
+  Daempfung.left := Masse.left;
+
+  Berechnen.height := Masse.height;
+  Berechnen.width := selectType.width;
+  Berechnen.left := selectType.Left;
+  Berechnen.top := selectType.top + selectType.height + Berechnen.height div 2;
+
+  GraphErstellen.height := Berechnen.height;
+  GraphErstellen.left := Berechnen.left;
+  GraphErstellen.width := Berechnen.width;
+  GraphErstellen.top := Berechnen.top + Berechnen.height;
 end;
 
 procedure TForm1.FormShow(Sender: TObject);
 begin
   WindowState := wsMaximized;
+  values.rowcount:=19999;
+  values.colcount:=4;
 end;
 
 procedure TForm1.GraphErstellenClick(Sender: TObject);
 var j: integer;
 begin
-   for j:=0 to 9999 do begin
+   for j:=0 to 19998 do begin
        with graph do begin
           if selectType.itemindex = 0 then begin
              canvas.Pixels[j+1, graph.height div 2 + round(strtofloat(values.cells[1, j])*10000)] := clred;
@@ -93,7 +137,7 @@ end;
 procedure TForm1.BerechnenClick(Sender: TObject);
 
 var i: integer;
-    m,D,y,k,v,a,t: real;
+    m,D,y,k,v,a,t, tsum: real;
 begin
 
 if selectType.ItemIndex = 0 then begin
@@ -101,13 +145,15 @@ if selectType.ItemIndex = 0 then begin
   D := strtofloat(Federkonstante.Text);
   y := strtofloat(Elongation.Text);
   v := 0;
-
-  for i:=0 to 9999 do begin
-         t := 0.001;
+  tsum := 0;
+  for i:=0 to 19998 do begin
+         t := 0.0005;
          a := -(D/m)*y;
          v := v+a*t;
          y := y+v*t;
+         tsum := tsum + t;
          with values do begin
+            cells[0, i] := floattostr(tsum);
             cells[1, i] := floattostr(y);
             cells[2, i] := floattostr(v);
             cells[3, i] := floattostr(a);
@@ -121,13 +167,15 @@ if selectType.ItemIndex = 1 then begin
   y := strtofloat(Elongation.Text);
   k := strtofloat(Daempfung.text);
   v := 0;
-
-  for i:=0 to 9999 do begin
-         t := 0.001;
+  tsum := 0;
+  for i:=0 to 19998 do begin
+         t := 0.0005;
          a := -(D*y+k*v)/m;
          v := v+a*t;
          y := y+v*t;
+         tsum := tsum + t;
          with values do begin
+            cells[0, i] := floattostr(tsum);
             cells[1, i] := floattostr(y);
             cells[2, i] := floattostr(v);
             cells[3, i] := floattostr(a);
