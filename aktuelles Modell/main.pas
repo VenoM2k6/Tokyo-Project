a9c44786433ad756325b72e139d3fcb595b640af
VenoM2k6 <v.witschelinski@web.de>
2024-03-16 21:01:51 +0100

    fancy

--- aktuelles Modell/main.pas
+++ aktuelles Modell/main.pas
@@ -14,56 +14,20 @@ type
 
   TmainProgram = class(TForm)
     Calculate: TButton;
-    collapse1: TButton;
     collapse2: TButton;
     collapse3: TButton;
-    collapse4: TButton;
-    collapse5: TButton;
-    collapse6: TButton;
-    collapse7: TButton;
-    collapse8: TButton;
     damping2: TEdit;
     damping3: TEdit;
-    damping4: TEdit;
-    damping5: TEdit;
-    damping6: TEdit;
-    damping7: TEdit;
-    damping8: TEdit;
-    damping9: TEdit;
-    edit: TButton;
-    collapse: TButton;
     edit1: TButton;
+    collapse1: TButton;
     edit2: TButton;
     edit3: TButton;
-    edit4: TButton;
-    edit5: TButton;
-    edit6: TButton;
-    edit7: TButton;
-    edit8: TButton;
     elongation2: TEdit;
     elongation3: TEdit;
-    elongation4: TEdit;
-    elongation5: TEdit;
-    elongation6: TEdit;
-    elongation7: TEdit;
-    elongation8: TEdit;
-    elongation9: TEdit;
     featherKonstant2: TEdit;
     featherKonstant3: TEdit;
-    featherKonstant4: TEdit;
-    featherKonstant5: TEdit;
-    featherKonstant6: TEdit;
-    featherKonstant7: TEdit;
-    featherKonstant8: TEdit;
-    featherKonstant9: TEdit;
     function2: TGroupBox;
     function3: TGroupBox;
-    function4: TGroupBox;
-    function5: TGroupBox;
-    function6: TGroupBox;
-    function7: TGroupBox;
-    function8: TGroupBox;
-    function9: TGroupBox;
     Graph: TImage;
     RecommendationMasse: TLabel;
     RecommandationLength: TLabel;
@@ -73,29 +37,11 @@ type
     RecommandationDamping: TLabel;
     length2: TEdit;
     length3: TEdit;
-    length4: TEdit;
-    length5: TEdit;
-    length6: TEdit;
-    length7: TEdit;
-    length8: TEdit;
-    length9: TEdit;
     locationFactor2: TEdit;
     locationFactor3: TEdit;
-    locationFactor4: TEdit;
-    locationFactor5: TEdit;
-    locationFactor6: TEdit;
-    locationFactor7: TEdit;
-    locationFactor8: TEdit;
-    locationFactor9: TEdit;
     MainMenu1: TMainMenu;
-    mass1: TEdit;
     mass2: TEdit;
     mass3: TEdit;
-    mass4: TEdit;
-    mass5: TEdit;
-    mass6: TEdit;
-    mass7: TEdit;
-    mass8: TEdit;
     RefreshMenu: TMenuItem;
     StringGrid1: TStringGrid;
     Tutorial: TMenuItem;
@@ -105,42 +51,35 @@ type
     nameFunction1: TEdit;
     nameFunction2: TEdit;
     nameFunction3: TEdit;
-    nameFunction4: TEdit;
-    nameFunction5: TEdit;
-    nameFunction6: TEdit;
-    nameFunction7: TEdit;
-    nameFunction8: TEdit;
-    nameFunction9: TEdit;
-    setType: TComboBox;
+    setType1: TComboBox;
     deleteAll: TButton;
     createNewFunction: TButton;
-    mass: TEdit;
+    mass1: TEdit;
     length1: TEdit;
     featherKonstant1: TEdit;
     elongation1: TEdit;
     locationFactor1: TEdit;
     damping1: TEdit;
     function1: TGroupBox;
-    setType1: TComboBox;
     setType2: TComboBox;
     setType3: TComboBox;
-    setType4: TComboBox;
-    setType5: TComboBox;
-    setType6: TComboBox;
-    setType7: TComboBox;
-    setType8: TComboBox;
     procedure CalculateClick(Sender: TObject);
     procedure CloseMenuClick(Sender: TObject);
-    procedure collapseClick(Sender: TObject);
+    procedure collapse1Click(Sender: TObject);
+    procedure collapse2Click(Sender: TObject);
+    procedure collapse3Click(Sender: TObject);
     procedure createNewFunctionClick(Sender: TObject);
     procedure deleteAllClick(Sender: TObject);
-    procedure editClick(Sender: TObject);
+    procedure edit1Click(Sender: TObject);
+    procedure edit2Click(Sender: TObject);
+    procedure edit3Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure GraphClick(Sender: TObject);
-    procedure locationFactor1Change(Sender: TObject);
-    procedure RecommandationLocationFactorClick(Sender: TObject);
-    procedure RecommandationElongationClick(Sender: TObject);
-    procedure ListBox1Click(Sender: TObject);
+    procedure nameFunction1Click(Sender: TObject);
+    procedure nameFunction1EditingDone(Sender: TObject);
+    procedure nameFunction2Click(Sender: TObject);
+    procedure nameFunction2EditingDone(Sender: TObject);
+    procedure nameFunction3Click(Sender: TObject);
+    procedure nameFunction3EditingDone(Sender: TObject);
     procedure RefreshMenuClick(Sender: TObject);
   private
 
@@ -151,6 +90,9 @@ type
 var
   mainProgram: TmainProgram;
   functionCreated: array[0..8] of Boolean;
+
+  //variables to fix bugs
+  nameWasAlreadyOverwritten1,nameWasAlreadyOverwritten2,nameWasAlreadyOverwritten3: boolean;
 
 implementation
 
@@ -163,42 +105,77 @@ begin
   WindowState := wsmaximized;
 end;
 
-procedure TmainProgram.GraphClick(Sender: TObject);
+//Editfields of function 1
+procedure TmainProgram.nameFunction1Click(Sender: TObject);
 begin
+  nameFunction1.text := '';
+  nameWasAlreadyOverwritten1 := false;
+end;
+procedure TmainProgram.nameFunction1EditingDone(Sender: TObject);
+begin
+  if nameWasAlreadyOverwritten1 = false then begin //This if clause & Variable fixxes the bug of overwriting 'Name' onto the function1.caption
+     function1.caption := namefunction1.text;
+  end;
+  nameFunction1.text := 'Name';
+  nameWasAlreadyOverwritten1 := true;
 
+  //fix selection bug
+  nameFunction1.enabled := false;
+  nameFunction1.enabled := true;
 end;
 
-procedure TmainProgram.locationFactor1Change(Sender: TObject);
+//Editfields of function 2
+procedure TmainProgram.nameFunction2Click(Sender: TObject);
 begin
+  nameFunction2.text:='';
+  nameWasAlreadyOverwritten2 := false;
+end;
+procedure TmainProgram.nameFunction2EditingDone(Sender: TObject);
+begin
+  if nameWasAlreadyOverwritten2 = false then begin //This if clause & Variable fixxes the bug of overwriting 'Name' onto the function1.caption
+     function2.caption := namefunction2.text;
+  end;
+  nameFunction2.text := 'Name';
+  nameWasAlreadyOverwritten2 := true;
 
+  //fix selection bug
+  nameFunction2.enabled := false;
+  nameFunction2.enabled := true;
 end;
 
-procedure TmainProgram.RecommandationLocationFactorClick(Sender: TObject);
+//Editfields of function 3
+procedure TmainProgram.nameFunction3Click(Sender: TObject);
 begin
+  nameFunction3.text := '';
+  nameWasAlreadyOverwritten3 := false;
+end;
+procedure TmainProgram.nameFunction3EditingDone(Sender: TObject);
+begin
+  if nameWasAlreadyOverwritten3 = false then begin //This if clause & Variable fixxes the bug of overwriting 'Name' onto the function1.caption
+     function3.caption := namefunction3.text;
+  end;
+  nameFunction3.text := 'Name';
+  nameWasAlreadyOverwritten3 := true;
 
+  //fix selection bug
+  nameFunction3.enabled := false;
+  nameFunction3.enabled := true;
 end;
 
-procedure TmainProgram.RecommandationElongationClick(Sender: TObject);
-begin
 
-end;
-
-procedure TmainProgram.ListBox1Click(Sender: TObject);
-begin
-
-end;
 
 procedure TmainProgram.RefreshMenuClick(Sender: TObject);
 begin
-  ShowMessage ('Flogt demnächst');
+  ShowMessage ('Folgt demnächst');
 end;
 
+//Create / Delete Functions
 procedure TmainProgram.createNewFunctionClick(Sender: TObject);
 var
   i: integer;
 begin
   //prüft und legt fest, ob eine Group Box schon benutzt wurde und zeigt die gewünschte an
-  for i:=0 to 8 do begin
+  for i:=0 to 2 do begin
     if functionCreated[i] = false then begin
        functionCreated[i] := true;
        if i = 0 then begin
@@ -210,27 +187,9 @@ begin
        if i = 2 then begin
           function3.visible := true;
        end;
-       if i = 3 then begin
-          function4.visible := true;
-       end;
-       if i = 4 then begin
-          function5.visible := true;
-       end;
-       if i = 5 then begin
-          function6.visible := true;
-       end;
-       if i = 6 then begin
-          function7.visible := true;
-       end;
-       if i = 7 then begin
-          function8.visible := true;
-       end;
-       if i = 8 then begin
-          function9.visible := true;
-       end;
        exit;
     end;
-    if functionCreated[8] = true then begin
+    if functionCreated[2] = true then begin
        showmessage('You have reached the maximum of functions! You can either delete all by pressing "Delete All" or you press the delete Button on a specific function');
        exit;
     end;
@@ -240,30 +199,53 @@ procedure TmainProgram.deleteAllClick(Sender: TObject);
 var
   i: integer;
 begin
-   for i:=0 to 8 do begin
+   for i:=0 to 12do begin
      functionCreated[i] := false;
    end;
    function1.visible := false;
    function2.visible := false;
    function3.visible := false;
-   function4.visible := false;
-   function5.visible := false;
-   function6.visible := false;
-   function7.visible := false;
-   function8.visible := false;
-   function9.visible := false;
 end;
 
-procedure TmainProgram.editClick(Sender: TObject);
+//maximize / minimze
+procedure TmainProgram.edit1Click(Sender: TObject);
 begin
    function1.height := 184;
-   edit.visible := false;
+   edit1.visible := false;
+   function2.Top := function1.top + function1.height;
+   function3.top := function2.top + function2.height;
 end;
-procedure TmainProgram.collapseClick(Sender: TObject);
+procedure TmainProgram.edit2Click(Sender: TObject);
 begin
-   function1.height := 31;
-   edit.visible := true;
+   function2.height := 184;
+   edit2.visible := false;
+   function3.top := function2.top + function2.height;
+
 end;
+procedure TmainProgram.edit3Click(Sender: TObject);
+begin
+   function3.height := 184;
+   edit3.visible := false;
+end;
+procedure TmainProgram.collapse1Click(Sender: TObject);
+begin
+   function1.height := edit1.height;
+   edit1.visible := true;
+   function2.Top := function1.top + function1.height;
+   function3.top := function2.top + function2.height;
+end;
+procedure TmainProgram.collapse2Click(Sender: TObject);
+begin
+   function2.height := edit2.height;
+   edit2.visible := true;
+   function3.top := function2.top + function2.height;
+end;
+procedure TmainProgram.collapse3Click(Sender: TObject);
+begin
+   function3.height := edit3.height;
+   edit3.visible := true;
+end;
+
 
 procedure TmainProgram.CloseMenuClick(Sender: TObject);
 begin
